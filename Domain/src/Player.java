a792f923edca14615790d88d93c6b1ec47052029
Santiago Gualdron <santiago.gualdron-r@mail.escuelaing.edu.co>
2024-05-01 18:29:54 -0500

    actualizar player con table

--- Domain/src/Player.java
+++ Domain/src/Player.java
@@ -4,19 +4,9 @@ import java.awt.Color;
 public class Player {
     private int cantWalls;
     private Color color;
-    private Box[][] casillas;
-    private String[][] typeCasillas;
 
     public Player(int nWalls, Color newColor){
         cantWalls = nWalls;
         color = newColor;
-        casillas = new Box[nWalls][nWalls];
-        typeCasillas = new String[nWalls][nWalls];
-        for (int i = 0; i < casillas.length; i++){
-            for (int j = 0; j < casillas.length; j++){
-                casillas[i][j] = new Box();
-                typeCasillas[i][j] = "normal";
-            }
-        }
     }
 }
